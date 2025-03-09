e9c29563c1a2a54ec2d2a928ccb6c6ad0ec10634
getGreat101 <64514277+getGreat101@users.noreply.github.com>
2025-03-09 13:49:30 +0800

    Update data_process.py

--- data/Tmall/data_process.py
+++ data/Tmall/data_process.py
@@ -42,7 +42,7 @@ def generate_interact(path):
     click_dict = generate_dict(path, 'click.txt')
     with open(os.path.join(path, 'click_dict.txt'), 'w', encoding='utf-8') as f:
         f.write(json.dumps(click_dict))
-
+    
     collect_dict = generate_dict(path, 'collect.txt')
     with open(os.path.join(path, 'collect_dict.txt'), 'w', encoding='utf-8') as f:
         f.write(json.dumps(collect_dict))
